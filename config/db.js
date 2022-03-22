8fa9b729effc31adb6a77f3b5088975ee5daf158
Pablo <pabloandres081200@gmail.com>
2022-03-21 22:01:15 -0300

    commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: "mongodb+srv://pablo33:<25120081200p@>@techphone.lpnwc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://pablo:25120081200p@@cluster0.9fspo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/techphone"}
 }
